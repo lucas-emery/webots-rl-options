0964351fda1e8f0a9f8ba5bc8fbda07c78bc126c
frandelgado <fdelgado@itba.edu.ar>
2020-07-11 01:26:47 -0300

    Refactor environment setup out of supervisor code

---
+++ epuck-nav/controllers/supervisor_controller/environment/__init__.py
@@ -0,0 +1,119 @@
+import random
+
+from pandas import np
+
+from utils.env_objects import Cylinder, Cube
+
+class EnvDefs:
+    epuck = ('EPUCK', 'webots_objects/E-puck.wbo')
+    cylinders = [
+        # node DEF,  node file definition, radius
+        ('Cylinder1', 'webots_objects/Cylinder1.wbo', 0.05),
+        ('Cylinder2', 'webots_objects/Cylinder2.wbo', 0.05)
+    ]
+    boxes = [
+        # node DEF,  node file definition, side length
+        ('Box1', 'webots_objects/Box1.wbo', 0.1)
+    ]
+
+
+class SimpleArena:
+
+    def __init__(self, supervisor, robot):
+        self.supervisor = supervisor
+        self.robot = robot
+        # initialization helper variables
+        self.robot_initial_position = []
+        self.children_field = None
+
+        # Respawn robot in starting position and state
+        root_node = self.supervisor.getRoot()  # This gets the root of the scene tree
+        self.children_field = root_node.getField(
+            'children')  # This gets a list of all the children, ie. objects of the scene
+        epuck_def, epuck_file = EnvDefs.epuck
+        self.children_field.importMFNode(-2, epuck_file)  # Load robot from file and add to second-to-last position
+        self.robot = self.supervisor.getFromDef(epuck_def)
+
+        self._insert_robot_in_random_position()
+        self.envorinment_objects = self._populate_environment_objects()
+
+    def get_environment_objects(self):
+        return self.envorinment_objects
+
+    def get_robot(self):
+        return self.robot
+
+    def _populate_environment_objects(self):
+        environment_objects = []
+
+        for node_def, node_file, radius in EnvDefs.cylinders:
+            wrapped_object = Cylinder(node_def, node_file, radius=radius)
+            self._place_object_in_random_position(environment_objects, wrapped_object)
+            environment_objects.append(wrapped_object)
+
+        for node_def, node_file, side_length in EnvDefs.boxes:
+            wrapped_object = Cube(node_def, node_file, side_length=side_length)
+            self._place_object_in_random_position(environment_objects, wrapped_object)
+            environment_objects.append(wrapped_object)
+
+        return environment_objects
+
+    def _place_object_in_random_position(self, placed_objects, wrapped_object):
+        """
+        Sets the shape passed by parameter to a random valid position
+        within the parent's node environment.
+
+        :param wrapped_object: the wrapped object with utility functions that is to be placed
+        :param placed_objects: the objects that have already been placed
+        :return: the node corresponding to the shape
+        """
+        self.children_field.importMFNode(-1, wrapped_object.node_file)
+        shape = self.supervisor.getFromDef(wrapped_object.node_def)
+        wrapped_object.webot_object = shape
+
+        x, z = self._generate_random_valid_position(placed_objects, wrapped_object)
+
+        trans_field = shape.getField('translation')
+        initial_position = [x, 0.05, z]
+        wrapped_object.initial_position = initial_position
+        trans_field.setSFVec3f(initial_position)
+        shape.resetPhysics()
+
+        return wrapped_object
+
+    def _generate_random_valid_position(self, placed_objects, wrapped_object):
+        valid_position_found = False
+        min_distance_from_wall = wrapped_object.get_min_distance_from_wall()
+        position_x = None
+        position_z = None
+        while not valid_position_found:
+            position_x, position_z = self._get_random_coords_in_arena(min_distance_from_wall)
+
+            if self._intersects_with_robot(position_x, position_z):
+                continue
+
+            valid_position_found = True
+            for placed_object in placed_objects:
+                if placed_object.is_inside_object(position_x, position_z):
+                    valid_position_found = False
+                    continue
+
+        return position_x, position_z
+
+    @staticmethod
+    def _get_random_coords_in_arena(min_distance_from_wall):
+        floor_x = 1 / 2
+        floor_z = 1 / 2
+        position_x = random.uniform(-floor_x + min_distance_from_wall, floor_x - min_distance_from_wall)
+        position_z = random.uniform(-floor_z + min_distance_from_wall, floor_z - min_distance_from_wall)
+        return position_x, position_z
+
+    def _intersects_with_robot(self, position_x, position_z):
+        position_vec = self.robot_initial_position
+        return np.sqrt(((position_vec[0] - position_x) ** 2) + ((position_vec[2] - position_z) ** 2)) < 0.1
+
+    def _insert_robot_in_random_position(self):
+        trans_field = self.robot.getField('translation')
+        x, z = self._get_random_coords_in_arena(0.045)
+        self.robot_initial_position = [x, 0.01, z]
+        trans_field.setSFVec3f(self.robot_initial_position)
--- epuck-nav/controllers/supervisor_controller/supervisor_controller.py
+++ epuck-nav/controllers/supervisor_controller/supervisor_controller.py
@@ -7,20 +7,9 @@ from typing import Optional
 from controller import Node
 from agents.tabular_agent import TabularAgent
 import pickle
+
+from environment import SimpleArena
 from utils.env_objects import Cylinder, Cube
-
-
-class EnvDefs:
-    epuck = ('EPUCK', 'webots_objects/E-puck.wbo')
-    cylinders = [
-        # node DEF,  node file definition, radius
-        ('Cylinder1', 'webots_objects/Cylinder1.wbo', 0.05),
-        ('Cylinder2', 'webots_objects/Cylinder2.wbo', 0.05)
-    ]
-    boxes = [
-        # node DEF,  node file definition, side length
-        ('Box1', 'webots_objects/Box1.wbo', 0.1)
-    ]
 
 
 class EpuckSupervisor(SupervisorCSV):
@@ -40,9 +29,6 @@ class EpuckSupervisor(SupervisorCSV):
         self.environment_objects = []
         self.message_received = None    # Variable to save the messages received from the robot
 
-        # initialization helper variables
-        self.robot_initial_position = []
-
     def reset_env(self):
         self.reward_tiles = np.random.uniform(size=self.reward_tiles.shape) < 0.2
 
@@ -54,94 +40,10 @@ class EpuckSupervisor(SupervisorCSV):
             if environment_object.webot_object:
                 environment_object.webot_object.remove()
 
-        # Respawn robot in starting position and state
-        root_node = self.supervisor.getRoot()            # This gets the root of the scene tree
-        children_field = root_node.getField('children')  # This gets a list of all the children, ie. objects of the scene
-        epuck_def, epuck_file = EnvDefs.epuck
-        children_field.importMFNode(-2, epuck_file)    # Load robot from file and add to second-to-last position
-        self.robot = self.supervisor.getFromDef(epuck_def)
-
-        #insert robot in random position
-        self.insert_robot_in_random_position()
-
-        self.environment_objects = self.populate_environment_objects(children_field)
-
-    def populate_environment_objects(self, children_field):
-        environment_objects = []
-
-        for node_def, node_file, radius in EnvDefs.cylinders:
-            wrapped_object = Cylinder(node_def, node_file, radius=radius)
-            self.place_object_in_random_position(environment_objects, wrapped_object, children_field)
-            environment_objects.append(wrapped_object)
-
-        for node_def, node_file, side_length in EnvDefs.boxes:
-            wrapped_object = Cube(node_def, node_file, side_length=side_length)
-            self.place_object_in_random_position(environment_objects, wrapped_object, children_field)
-            environment_objects.append(wrapped_object)
-
-        return environment_objects
-
-    def place_object_in_random_position(self, placed_objects, wrapped_object, children_field):
-        """
-        Sets the shape passed by parameter to a random valid position
-        within the parent's node environment.
-
-        :param wrapped_object: the wrapped object with utility functions that is to be placed
-        :param placed_objects: the objects that have already been placed
-        :param children_field: the node to which the shape will be attached to
-        :return: the node corresponding to the shape
-        """
-        children_field.importMFNode(-1, wrapped_object.node_file)
-        shape = self.supervisor.getFromDef(wrapped_object.node_def)
-        wrapped_object.webot_object = shape
-
-        x, z = self.generate_random_valid_position(placed_objects, wrapped_object)
-
-        trans_field = shape.getField('translation')
-        initial_position = [x, 0.05, z]
-        wrapped_object.initial_position = initial_position
-        trans_field.setSFVec3f(initial_position)
-        shape.resetPhysics()
-
-        return wrapped_object
-
-    def generate_random_valid_position(self, placed_objects, wrapped_object):
-
-        valid_position_found = False
-        min_distance_from_wall = wrapped_object.get_min_distance_from_wall()
-        position_x = None
-        position_z = None
-        while not valid_position_found:
-            position_x, position_z = self.get_random_coords_in_arena(min_distance_from_wall)
-
-            if self.intersects_with_robot(position_x, position_z):
-                continue
-
-            valid_position_found = True
-            for placed_object in placed_objects:
-                if placed_object.is_inside_object(position_x, position_z):
-                    valid_position_found = False
-                    continue
-
-        return position_x, position_z
-
-    @staticmethod
-    def get_random_coords_in_arena(min_distance_from_wall):
-        floor_x = 1 / 2
-        floor_z = 1 / 2
-        position_x = random.uniform(-floor_x + min_distance_from_wall, floor_x - min_distance_from_wall)
-        position_z = random.uniform(-floor_z + min_distance_from_wall, floor_z - min_distance_from_wall)
-        return position_x, position_z
-
-    def intersects_with_robot(self, position_x, position_z):
-        position_vec = self.robot_initial_position
-        return np.sqrt(((position_vec[0] - position_x) ** 2) + ((position_vec[2] - position_z) ** 2)) < 0.1
-
-    def insert_robot_in_random_position(self):
-        trans_field = self.robot.getField('translation')
-        x, z = self.get_random_coords_in_arena(0.045)
-        self.robot_initial_position = [x, 0.01, z]
-        trans_field.setSFVec3f(self.robot_initial_position)
+        # Initialize environment
+        simple_arena_environment = SimpleArena(self.supervisor, self.robot)
+        self.environment_objects = simple_arena_environment.get_environment_objects()
+        self.robot = simple_arena_environment.get_robot()
 
     def get_observations(self):
         observations = []
